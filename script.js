f53dc25529d9f3687aebb236eb117a348dd0e723
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-29 16:47:56 +0100

    Create getNewDisplay() to simplify eventListeners and link function to all non-zero numerical buttons

--- script.js
+++ script.js
@@ -4,11 +4,55 @@ const display = document.getElementById("display");
 
 const buttonOne = document.getElementById("btn1");
 buttonOne.addEventListener("click", () => {
-    displayValue = displayValue + "1";
-    let newDisplayValue = document.createTextNode(displayValue);
-    display.removeChild(display.lastChild)
-    display.appendChild(newDisplayValue);
+    getNewDisplay("1");
 })
+
+const buttonTwo = document.getElementById("btn2");
+buttonTwo.addEventListener("click", () => {
+    getNewDisplay("2");
+})
+
+const buttonThree = document.getElementById("btn3");
+buttonThree.addEventListener("click", () => {
+    getNewDisplay("3");
+})
+
+const buttonFour = document.getElementById("btn4");
+buttonFour.addEventListener("click", () => {
+    getNewDisplay("4");
+})
+
+const buttonFive = document.getElementById("btn5");
+buttonFive.addEventListener("click", () => {
+    getNewDisplay("5");
+})
+
+const buttonSix = document.getElementById("btn6");
+buttonSix.addEventListener("click", () => {
+    getNewDisplay("6");
+})
+
+const buttonSeven = document.getElementById("btn7");
+buttonSeven.addEventListener("click", () => {
+    getNewDisplay("7");
+})
+
+const buttonEight = document.getElementById("btn8");
+buttonEight.addEventListener("click", () => {
+    getNewDisplay("8");
+})
+
+const buttonNine = document.getElementById("btn9");
+buttonNine.addEventListener("click", () => {
+    getNewDisplay("9");
+})
+
+function getNewDisplay (newString) {
+    displayValue = displayValue + newString;
+    let newDisplay = document.createTextNode(displayValue);
+    display.removeChild(display.lastChild)
+    display.appendChild(newDisplay);
+};
 
 function calculateAnswer(firstNumber, operator, secondNumber){
     if (operator == "+"){
