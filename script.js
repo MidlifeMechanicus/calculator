4dd2758c6501218225d87486185dc99f575b50b7
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-30 11:23:22 +0100

    Complete header and footer and Rearrange html for keyPad

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let operatorInput;
 let secondInput;
 
 // Todo: html/css (obviously), rounding, double operater issue, decimal? keystrokes, backspace button, /0 error message in display rather than alert, header with links
+// CODE ISSUE WITH CARRY NUMBER FORWARD
+
 
 const inputDisplay = document.getElementById("inputDisplay");
 const calculationDisplay = document.getElementById("calculationDisplay");
